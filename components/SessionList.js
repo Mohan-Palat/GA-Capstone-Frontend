d878c548de7f39788a95514cfe3d67709ad35db4
joshuasuzuki <joshuakai.suzuki@gmail.com>
2020-12-15 20:52:07 -0500

    initial revision

---
+++ components/SessionList.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class SessionList extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            sessions: []
+        }
+    }
+
+    getAllSessions = () =>{
+        const apiURL = '';
+    }
+
+    render(){
+        return(
+            <div>
+            </div>
+        );
+    }
+}
+
+export default SessionList;
