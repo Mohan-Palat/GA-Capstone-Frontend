97642ba7bcac00de89d0d01b44509787702ee516
joshuasuzuki <joshuakai.suzuki@gmail.com>
2020-12-18 02:34:14 -0500

    show hand wip

--- src/components/Hand.js
+++ src/components/Hand.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
 function Hand(props){
-  
+    const deltaClass = props.hand.delta >=0 ? 'positive':'negative'
+    console.log('DELTA: ',deltaClass)
     return(
         <div>
            {props.hand.hole_cards[0].rank}{props.hand.hole_cards[0].suit}{props.hand.hole_cards[1].rank}{props.hand.hole_cards[1].suit}
+           <div className="positive">{props.hand.delta}</div>
         </div>
     );
 }
